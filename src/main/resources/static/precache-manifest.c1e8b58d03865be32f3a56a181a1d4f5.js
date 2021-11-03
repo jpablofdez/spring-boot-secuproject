7bf3e1a3a9888eeadbbf90db8bfe205d83bcd4ed
jpablofdez <jpablofdez@localhost>
2021-11-02 23:22:27 -0600

    update static

---
+++ src/main/resources/static/precache-manifest.c1e8b58d03865be32f3a56a181a1d4f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4f6cdb8b973b95ac6f4",
+    "url": "/static/css/main.b5169cf4.chunk.css"
+  },
+  {
+    "revision": "b4f6cdb8b973b95ac6f4",
+    "url": "/static/js/main.1f6fa204.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d89fbe40ceda3123534a",
+    "url": "/static/css/2.065ba17a.chunk.css"
+  },
+  {
+    "revision": "d89fbe40ceda3123534a",
+    "url": "/static/js/2.0ae7d15a.chunk.js"
+  },
+  {
+    "revision": "0d127e1f93f15c571b1439e60e0e9a7c",
+    "url": "/index.html"
+  }
+];
